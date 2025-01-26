5ffc3d6162b792e85845e1fa33882a53631cc272
parasnever <parasshresthanever@gmail.com>
2025-01-26 17:14:50 +0545

    remove slash

--- src/services/mongo-db.ts
+++ src/services/mongo-db.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import mongoose from "mongoose";
-const db = mongoose.createConnection("mongodb://localhost:27017/", {
+const db = mongoose.createConnection("mongodb://localhost:27017", {
   dbName: "movie-review-app",
 });
 console.log("MongoDb Database connected!!:");
