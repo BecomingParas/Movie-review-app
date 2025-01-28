f2203df981c79260b2c278184dd019cc1e3cc8ff
parasnever <parasshresthanever@gmail.com>
2025-01-29 02:53:00 +0545

    Enhance movie creation controller with error handling and success response

--- src/controllers/movie-review-controllers/movie-controllers/create-movie-controller.ts
+++ src/controllers/movie-review-controllers/movie-controllers/create-movie-controller.ts
@@ -3,36 +3,55 @@ import { movieService } from "../../../services/movie";
 import { CreateMovieSchema } from "../../../services/movie-review-validations";
 import { InvalidMovieReviewPayload } from "../../../services/movie-review-errors";
 import { movieMongoService } from "../../../mongo/movie/service";
+import { MovieReviewAppError } from "../../../error";
 export async function createMovieController(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const body = req.body;
-  const parsed = CreateMovieSchema.safeParse(body);
-  if (!parsed.success) {
-    const parseError = parsed.error.flatten();
-    const invalidPayloadError = new InvalidMovieReviewPayload(parseError);
-    next(invalidPayloadError);
-    return;
-  }
+  try {
+    const body = req.body;
+    const parsed = CreateMovieSchema.safeParse(body);
+    if (!parsed.success) {
+      const parseError = parsed.error.flatten();
+      const invalidPayloadError = new InvalidMovieReviewPayload(parseError);
+      next(invalidPayloadError);
+      return;
+    }
 
-  if (process.env.DATABASE_TYPE === "MYSQL") {
-    movieService.createMovie({
-      title: parsed.data.title,
-      description: parsed.data.description,
-      release_year: parsed.data.release_year,
-      genre: parsed.data.genre,
+    if (process.env.DATABASE_TYPE === "MYSQL") {
+      movieService.createMovie({
+        title: parsed.data.title,
+        description: parsed.data.description,
+        release_year: parsed.data.release_year,
+        genre: parsed.data.genre,
+      });
+    } else {
+      await movieMongoService.createMovie({
+        title: parsed.data.title,
+        description: parsed.data.description,
+        genre: parsed.data.genre,
+        release_year: parsed.data.release_year,
+      });
+    }
+    res.json({
+      message: "Movie added successfully.",
     });
-  } else {
-    await movieMongoService.createMovie({
-      title: parsed.data.title,
-      description: parsed.data.description,
-      genre: parsed.data.genre,
-      release_year: parsed.data.release_year,
-    });
+  } catch (error) {
+    console.error(error);
+    if ((error as any).errorResponse.code === 11000) {
+      const movieError = new MovieReviewAppError(
+        "Failed to create the movie.please choose unique title",
+        400
+      );
+      next(movieError);
+      return;
+    }
+
+    const movieError = new MovieReviewAppError(
+      "Failed to create the movie. something went wrong in server",
+      500
+    );
+    next(movieError);
   }
-  res.json({
-    message: "Movie added successfully.",
-  });
 }
