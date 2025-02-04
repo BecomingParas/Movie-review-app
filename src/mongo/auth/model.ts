71b62941184f5cc74854dd9ed333efcce19aa691
parasnever <parasshresthanever@gmail.com>
2025-02-04 14:56:27 +0545

    auth

--- src/mongo/auth/model.ts
+++ src/mongo/auth/model.ts
@@ -1,25 +1,25 @@
-// import mongoose from "mongoose";
-// import { string } from "zod";
+import mongoose from "mongoose";
+import { string } from "zod";
 
-// const userSchema = new mongoose.Schema({
-//   username: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//   },
-//   created_at: {
-//     type: Date,
-//     default: Date.now,
-//   },
-// });
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-// export const UserModel = mongoose.model("User", userSchema);
+export const UserModel = mongoose.model("User", userSchema);
