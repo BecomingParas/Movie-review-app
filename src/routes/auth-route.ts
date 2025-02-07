42fd658626f10edc6806f53799e8e32cee68425f
parasnever <parasshresthanever@gmail.com>
2025-02-07 21:29:24 +0545

    Add JWT secret to environment configuration and implement login route

--- src/routes/auth-route.ts
+++ src/routes/auth-route.ts
@@ -1,6 +1,8 @@
+import { loginController } from "../controllers/auth-controller/login-controller";
 import { signUpController } from "../controllers/auth-controller/signup-controller";
 import { Express } from "express";
 export function createAuthRoutes(app: Express) {
   // mutation
   app.post("/auth/signup", signUpController);
+  app.post("/auth/login", loginController);
 }
