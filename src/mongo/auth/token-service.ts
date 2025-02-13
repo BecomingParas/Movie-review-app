b373214ae22a811862c707e7f15d597e7aadac70
parasnever <parasshresthanever@gmail.com>
2025-02-14 00:20:47 +0545

    Add token creation and deletion functions to token service

--- src/mongo/auth/token-service.ts
+++ src/mongo/auth/token-service.ts
@@ -1,1 +1,40 @@
+import { TokenModel } from "./model";
+
+type TCreateTokenInput = {
+  userId: string;
+  token: string;
+};
+
+// create a token
+
+async function createToken(input: TCreateTokenInput) {
+  const token = new TokenModel({
+    user_id: input.userId,
+    token: input.token,
+  });
+  await token.save();
+}
+
+type TDeleteTokenInput = {
+  token: string;
+  userId: string;
+};
+
+//delete a token
+
+async function deleteToken(input: TDeleteTokenInput) {
+  await TokenModel.deleteOne({
+    user_id: input,
+    token: input.token,
+  });
+}
+
+
+// get Token 
+
+async function getToken(){
+    await TokenModel.
+}
+
+
 
