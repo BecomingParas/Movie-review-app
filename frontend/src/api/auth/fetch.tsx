9dde50907790dab47bf1c3d2bf0eaaf09b98051d
parasnever <parasshresthanever@gmail.com>
2025-02-25 15:10:38 +0545

    Implement login API with type definitions and error handling

--- frontend/src/api/auth/fetch.tsx
+++ frontend/src/api/auth/fetch.tsx
@@ -35,3 +35,42 @@ export async function registerUser(
   }
   return data;
 }
+
+// for login api
+export type TLoginUserInput = {
+  email: string;
+  password: string;
+};
+export type TUserRole = "admin" | "user";
+
+export type TLoginUserOutput = {
+  message: string;
+  isSuccess: boolean;
+  data: {
+    username: string;
+    email: string;
+    id: string;
+    role: TUserRole;
+  };
+};
+
+export async function loginUser(
+  input: TLoginUserInput
+): Promise<TLoginUserOutput> {
+  const res = await fetch(`${env.BACKEND_URL}/api/auth/login`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: input.email,
+      password: input.password,
+    }),
+  });
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.message);
+  }
+  return data;
+}
