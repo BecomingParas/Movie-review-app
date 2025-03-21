2c3385519f07c254f7909cfe4e93157691ba80aa
parasnever <parasshresthanever@gmail.com>
2025-03-21 14:43:09 +0545

    authentication login and signup working

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,7 +15,14 @@ connectMongoDb().then(() => {
 
 // json parser
 const app = express();
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 
