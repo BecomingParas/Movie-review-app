358376eb31d07838b36d5f0ff0ff3234e252d2e3
BecomingParas <parasshresthanever@gmail.com>
2025-06-13 11:45:52 +0545

    refactor: enhance CORS setup to use a dynamic origin callback

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -25,15 +25,21 @@ connectMongoDb().then(() => {
 // CORS Setup
 app.use(
   cors({
-    origin: [
-      "http://localhost:4200",
-      "https://movie-review-6u4tpbxic-menu-2a8d524d.vercel.app",
-    ],
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:4200",
+        "https://movie-review-6u4tpbxic-menu-2a8d524d.vercel.app",
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.options("*", cors());
 
 // Middleware
 app.use(cookieParser());
